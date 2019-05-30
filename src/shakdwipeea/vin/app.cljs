86789ed7beb5d5954afdcece73c3a50ee3ac3221
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2019-05-30 20:49:04 +0530

    and then there was light

--- src/shakdwipeea/vin/app.cljs
+++ src/shakdwipeea/vin/app.cljs
@@ -14,9 +14,11 @@
 
 ;; scene
 
-(defn create-scene [scenes]
+(defn create-scene [& scenes]
   (let [scene (t/Scene.)]
-    (doall (map #(.add scene %) scenes))
+    ;; (aset scene "background" (doto (t/Color.)
+    ;;                            (.setHSL 0.6 0 1)))
+    (when (seq? scenes) (doall (map #(.add scene %) scenes)))
     scene))
 
 ;; renderer
@@ -24,22 +26,33 @@
 (defn create-renderer []
   (doto (t/WebGLRenderer.)
     (.setPixelRatio (.-devicePixelRatio js/window))
-    (.setSize (width) (height))))
+    (.setSize (width) (height))
+    (aset "toneMapping" t/ReinhardToneMapping)))
 
 ;; lights
 
 (defn create-ambient-light [color intensity]
   (t/AmbientLight. color intensity))
 
-(defn create-point-light [color intensity {:keys [x y z]}]
-  (let [light (t/PointLight. color intensity)]
+(defn create-point-light [{[x y z] :position
+                           :keys [color intensity distance]
+                           :or {distance 100}
+                           :as p}]
+  (println x y z)
+  (let [light (t/PointLight. color intensity distance 2)]
+    (.set (.-position light) x y z)
+    light))
+
+(defn create-directional-light [{[x y z] :position
+                                 :keys [position color intensity]}]
+  (let [light (t/DirectionalLight. color intensity)]
     (.set (.-position light) x y z)
     light))
 
 ;; geometery primitives
 
-(defn create-box [w h d]
-  (t/BoxGeometry. w h d))
+(defn create-box [{:keys [width height depth]}]
+  (t/BoxGeometry. width height depth))
 
 (defn create-basic-material [color]
   (t/MeshBasicMaterial. #js {:color color}))
@@ -47,13 +60,43 @@
 (defn create-normal-material []
   (t/MeshNormalMaterial.))
 
-(defn create-mesh [g m] (t/Mesh. g m))
+(defn create-mesh-phong-material [{:keys [color dithering]
+                                   :or {color 0xffffee
+                                        dithering true}
+                                   :as opts}]
+  (t/MeshPhongMaterial. (clj->js opts)))
 
-(defn create-cube [w h d]
-  (create-mesh (create-box w h d)
+(defn create-standard-material
+  [& {:keys [emissive emissive-intensity color]
+      :or   {emissive 0xffffee
+             emissive-intensity 10
+             color 0xffffee}}]
+  (t/MeshStandardMaterial. (clj->js {:emissive emissive
+                                     :emissiveIntensity emissive-intensity
+                                     :color color})))
+
+(defn create-mesh [g m & {position :position}]
+  (let [m (t/Mesh. g m)]
+    (when-let [[x y z] position]
+      (-> m
+          .-position
+          (.set x y z)))
+    m))
+
+(defn create-cube [{:keys [width height depth] :as params}]
+  (create-mesh (create-box params)
                (create-basic-material 0x00234c)))
 
-(defn create-sphere [r w h] (t/SphereBufferGeometry. r w h))
+(defn create-sphere [{:keys [radius width height]}]
+  (t/SphereGeometry. radius width height))
+
+(defn create-bulb [{[x y z] :position
+                    :keys [radius width height color intensity]
+                    :as params}]
+  (doto (create-point-light params)
+    (.add (create-mesh (create-sphere params)
+                       (create-standard-material)))
+    (aset "castShadow" true)))
 
 (defn cb->chan
   ([] (cb->chan (a/chan 1)))
@@ -74,11 +117,13 @@
     ch))
 
 ;; camera
-
 (defn create-camera [] (t/PerspectiveCamera. 70
                                              (/ (width) (height))
                                              0.01
-                                             10))
+                                             1000))
+
+(defn add-light-to-camera! [camera light]
+  (.add camera light))
 
 ;; three js
 
@@ -119,7 +164,8 @@
   (.load (t/TextureLoader.) url))
 
 (defn add-scene-in-state [game-state scene]
-  (update game-state :scene conj scene))
+  (update game-state :scene (fn [s] (.add (or s
+                                             (create-scene)) scene) )))
 
 (defn game-loop [game-state chans]
   (a/go (loop [g game-state]
@@ -127,9 +173,9 @@
                 g1  (cond-> g
                       (= (v :type) :mesh)
                       (add-scene-in-state (v :mesh)))]
-            
+            (when (not= (v :type) :frame) (println v))
             (.render renderer
-                     (create-scene (:scene g1))
+                     (:scene g1)
                      (:camera g1))
             (recur g1)))))
 
@@ -145,6 +191,11 @@
                                       (frame-loop frame-chan))))
 
 
+(defn add-scene! [scene-chan mesh]
+  (go  (>! scene-chan
+           {:type :mesh
+            :mesh mesh})))
+
 (defn render-canvas [renderer dom-id]
   (let [canvas (.getElementById js/document dom-id)
         camera (create-camera)
@@ -157,39 +208,36 @@
                      (aget (.-childNodes canvas) 0))
       (.appendChild canvas (.-domElement renderer)))
     (println camera)
-    (aset camera "position" "z" 3)
+    (aset camera "position" "z" 30)
     (doto (orbit/OrbitControls. camera)
-      #(.set (.-target %) '(0 -0.2 -0.2))
+      #(.set camera '(0 -1 0))
       .update)
     ;; (add-event-listener "keyup" keydown-chan)
+    (doto scene-chan
 
-    (go  (>! scene-chan
-             {:type :mesh
-              :mesh (let [l (t/PointLight. 0xffffff 2 50)]
-                      (.add l
-                            (create-mesh (create-sphere 0.5 1 1)
-                                         (create-basic-material 0xff0040)))
-                      (.set (.-position l) 4 4 4)
-                      l)}))
+      (add-scene! (create-bulb {:radius    1
+                                :width     32
+                                :height    32
+                                :position  [0 2 0]
+                                :color     0xffffee
+                                :intensity 100}))
 
-    ;; (go (>! scene-chan {:type :scene
-    ;;                     :scene (t/AmbientLight. 0xcccccc 0.4)}))
+      (add-scene! (t/HemisphereLight. 0xffffbb 0x080820 1))
 
-    ;; (go (>! scene-chan {:type :mesh
-    ;;                     :mesh (create-cube 0.2 0.2 0.2)}))
-    (def t (load-texture "texture.jpg"))
-    (a/pipeline 1
-                scene-chan
-                (map (fn [m]
-                       ;; (.traverse (.-scene m)
-                       ;;            (fn [child]
-                       ;;              (when (.-isMesh child)
-                       ;;                (aset child "material" "envmap"
-                       ;;                      t))))
-                       ;; (aset (.-scene m) "background" t)
-                       {:type :mesh
-                        :mesh (-> m .-scene)}))
-                (load-gltf-model "glTF/Duck.gltf"))
+
+      ;; (add-scene! (create-mesh (create-box {:width 3
+      ;;                                       :height 1
+      ;;                                       :depth 2})
+      ;;                          (create-mesh-phong-material {})
+      ;;                          {:position [0 -1 0]}))
+      )
+
+    ;; (a/pipeline 1
+    ;;             scene-chan
+    ;;             (map (fn [m]
+    ;;                    {:type :mesh
+    ;;                     :mesh (-> m .-scene)}))
+    ;;             (load-gltf-model "elendil.glb"))
     (game-loop {:camera camera
                 :start-time (js/Date.)}
                [frame-chan scene-chan])
